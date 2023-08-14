a15ea8d65584ddc19b7cd170a21b2257741be958
CJReynoso <140649611+CJReynoso@users.noreply.github.com>
2023-08-14 13:57:17 -0600

    Update index.js

--- final_project/index.js
+++ final_project/index.js
@@ -4,6 +4,7 @@ const session = require('express-session')
 const customer_routes = require('./router/auth_users.js').authenticated;
 const genl_routes = require('./router/general.js').general;
 const cors = require('cors');
+const { Cookie } = require('express-session');
 
 const authenticatedUser = (username,password)=>{
     let validusers = users.filter((user)=>{
@@ -18,40 +19,20 @@ const authenticatedUser = (username,password)=>{
 
 let users= [{"username":"Batu", "password":"123456"}]
 
+
 const app = express();
 
 app.use(cors());
 
 app.use(express.json());
 
-app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
+app.use("/customer",session({secret:"fingerprint_customer",resave: false, saveUninitialized: true,} ))
 
+/*
 app.use("/customer/auth/*", function auth(req,res,next){
 
-  const username = req.body.username;
-  const password = req.body.password;
-
-  if (!username || !password) 
-  {
-      return res.status(404).json({message: "Error logging in"});
-  }
-
-  if (authenticatedUser(username,password)) 
-  {
-    let accessToken = jwt.sign({
-      data: password
-    }, 'access', { expiresIn: 60  });
-
-        req.session.authorization = {accessToken,username}
-
-  return res.status(200).send("User successfully logged in");
-  } 
-  else 
-  {
-    return res.status(208).json({message: "Invalid Login. Check username and password"});
-  }
-
 });
+*/
  
 const PORT =5000;
 
