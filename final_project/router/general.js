6bfcc834ce6ccffd4cf6094a009e1072bd54c1f2
CJReynoso <140649611+CJReynoso@users.noreply.github.com>
2023-08-15 18:22:40 -0600

    Update general.js

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -4,6 +4,7 @@ let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
 
+
 const doesExist = (username)=>{
     let userswithsamename = users.filter((user)=>{
       return user.username === username
@@ -31,17 +32,35 @@ public_users.post("/register", (req,res) => {
     return res.status(404).json({message: "Unable to register user."});
   });
 
-// Get the book list available in the shop
-public_users.get('/',function (req, res) {
-  //Return the list of Books
-  return res.send(JSON.stringify(books));
-});
 
+// Get the book list available in the shop
+public_users.get('/', function (req, res) {
+    //Return the list of Books
+    return res.send(JSON.stringify(books));
+  });
+
+let asyncAllBooks= new Promise((resolve, reject) =>{
+        resolve(JSON.stringify(books))
+    })
+
+
+asyncAllBooks.then((successResponse) =>{
+    console.log(successResponse)
+})
+  
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
   //Return the details
   return res.send(books[req.params.isbn]);
  });
+
+ let asyncBooksByIsbn= new Promise((resolve,reject) =>{
+     resolve(books[5])
+ })
+
+ asyncBooksByIsbn.then((successResponse) =>{
+    console.log(successResponse)
+})
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
@@ -59,6 +78,24 @@ public_users.get('/author/:author',function (req, res) {
   return res.send(sbook);
 });
 
+let asyncBooksByAuthor= new Promise((resolve,reject) =>{
+    let author='Unknown'
+    let sbook;
+
+  for (let i=1; i<11; i++)
+  {
+      if(books[i].author=== author)
+      {
+          sbook=books[i]
+      }
+  }
+  resolve(sbook)
+})
+
+asyncBooksByAuthor.then((successResponse) =>{
+    console.log(successResponse)
+})
+
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
   //Return the details
@@ -74,6 +111,24 @@ public_users.get('/title/:title',function (req, res) {
 
   return res.send(sbook);
 });
+
+let asyncBooksByTitle= new Promise((resolve,reject) =>{
+    let title='Fairy tales'
+    let sbook;
+
+  for (let i=1; i<11; i++)
+  {
+      if(books[i].title=== title)
+      {
+          sbook=books[i]
+      }
+  }
+    resolve(sbook)
+})
+
+asyncBooksByTitle.then((successResponse) =>{
+    console.log(successResponse)
+})
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
